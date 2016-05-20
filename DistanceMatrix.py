c9b1c672b42c32af8fa3a86f4621b50b28712344
= <helenfoster2016@u.northwestern.edu>
2016-05-20 11:22:43 -0500

    Formats output to numpy arrays

--- DistanceMatrix.py
+++ DistanceMatrix.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sys
+import numpy as np
 
 def FormatParameters(parameters):
     NewParameters = ""
@@ -16,21 +17,44 @@ def BuildRequest(origins, destinations, key):
     return "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&" + origins + destinations + APIkey
 
 def ParseResponse(response):
-    print response.text
+    response = json.loads(response.text)['rows']
+    numOrigins = len(response)
+    numDestinations = len(response[0]['elements'])
+
+    #columns = destinations = elements
+    #rows = origins = rows
+    distanceMatrix = np.zeros((numOrigins, numDestinations), dtype='f')
+    durationMatrix = np.zeros((numOrigins, numDestinations), dtype="int")
+
+    for o in range(0, numOrigins):
+        for d in range(0, numDestinations):
+
+            #duration in seconds
+            duration = response[o]['elements'][d]['duration']['value']
+            #distance in meters
+            distance = response[o]['elements'][d]['distance']['value']
+
+            durationMatrix[o, d] = duration
+            distanceMatrix[o, d] = distance
+    print "duration matrix: "
+    print durationMatrix
+    print
+    print "distance matrix: "
+    print distanceMatrix
+
 
 def GetMatrix(origins, destinations, key):
     HTTPRequest = BuildRequest(origins, destinations, key)
-
     response = requests.get(HTTPRequest)
     return ParseResponse(response)
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        origins = ["Vancouver, BC, Canada", "Seattle, WA, USA"]
+        origins = ["Vancouver, BC, Canada", "Seattle, WA, USA", "Victoria, BC, Canada"]
         destinations = ["San Francisco, CA, USA", "Victoria, BC, Canada"]
-    else:
-        origins = sys.argv[0]
-        destinations = sys.argv[1]
+    #else:
+    #    origins = sys.argv[0]
+    #    destinations = sys.argv[1]
 
     APIKey = "AIzaSyDfNjjRUcKkycQ0vcQ1Vh0vJCCLppimCYQ"
     GetMatrix(origins, destinations, APIKey)
