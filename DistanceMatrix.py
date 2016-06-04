714123c31a753796a6da5d8c2ee94460d782fa05
Pooja Saxena <pooja335@gmail.com>
2016-06-04 03:08:55 -0500

    reading data from csv, parsing, finding info we need

--- DistanceMatrix.py
+++ DistanceMatrix.py
@@ -3,13 +3,22 @@ import json
 import sys
 import numpy as np
 import os
+import math
+import time
+import csv
+import re
 
 def FormatParameters(parameters):
-    NewParameters = ""
+    formatted_params = ""
     for param in parameters:
-        NewParameters += param.replace(", ", "+")
-        NewParameters += "|"
-    return NewParameters[:-1]
+        split_string = param.split(',')
+        split_string.reverse()
+        param = ','.join(split_string)
+        formatted_params += param + "|"
+    return formatted_params[:-1]
+
+    # pipe = "|"
+    # return pipe.join(parameters);
 
 def BuildRequest(origins, destinations, key):
     origins = "origins=" + FormatParameters(origins) + "&"
@@ -44,17 +53,62 @@ def ParseResponse(response):
     print distanceMatrix
 
 def GetMatrix(origins, destinations, key):
-    HTTPRequest = BuildRequest(origins, destinations, key)
-    response = requests.get(HTTPRequest)
+    max_request_params = 10;
+    num_requests_sqrt = int(math.ceil(len(origins)/float(max_request_params)))
+    for d in range(0, num_requests_sqrt):
+        current_destination = max_request_params * d
+        destinations_subset = destinations[current_destination:(current_destination + max_request_params)]
+        for o in range(0, num_requests_sqrt):
+            current_origin = max_request_params * o
+            origins_subset = origins[current_origin:(current_origin + max_request_params)]
+
+            HTTPRequest = BuildRequest(origins_subset, destinations_subset, key)
+            response = requests.get(HTTPRequest)
+            time.sleep(1)
+            print ParseResponse(response)
+            print
     return ParseResponse(response)
+    # Concatenate number matrices
+
+def GetData():
+    building_data = [];
+    with open('Buildings.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            #find text with number of stories
+            p = re.compile('STORIES<\/td><td>([\ \/0-9a-zA-Z]*)')
+            match = p.search(row[1]).group(1)
+            #check if number of stories data exists
+            if match:
+                if match == 'No Stories':
+                    num_stories = 0.0
+                elif match == '1/2 Stories':
+                    num_stories = 0.5
+                elif '/' in match:
+                    num_stories = float(match[0:2]) + 0.5
+                else:
+                    num_stories = float(match[0:2])
+
+                #get latitude and longitude data
+                lon = float(row[2].split(',')[0])
+                lat = float(row[2].split(',')[1])
+
+                #check if lat and lon are in evanston range
+                if lat < 42.062057 and lat > 42.048164 and lon < -87.678947 and lon > -87.710688:
+                    building_data.append({'lat': lat, 'lon': lon, 'stories': num_stories})
+    print building_data
+    print
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        origins = ["Vancouver, BC, Canada", "Seattle, WA, USA", "Victoria, BC, Canada"]
-        destinations = ["San Francisco, CA, USA", "Victoria, BC, Canada"]
+        # origins = ["Vancouver, BC, Canada", "Seattle, WA, USA", "Victoria, BC, Canada"]
+        # destinations = ["San Francisco, CA, USA", "Victoria, BC, Canada"]
+
+        GetData();
+
     #else:
     #    origins = sys.argv[0]
     #    destinations = sys.argv[1]
 
-    APIKey = os.environ['GMAP_KEY']
-    GetMatrix(origins, destinations, APIKey)
+    # APIKey = os.environ['GMAP_KEY']
+    # GetMatrix(origins, destinations, 'AIzaSyDfNjjRUcKkycQ0vcQ1Vh0vJCCLppimCYQ')
