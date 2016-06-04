4184bb675bb51a06c2e5adb2fa30d9f67bbfff3c
Pooja Saxena <pooja335@gmail.com>
2016-06-04 18:32:45 -0500

    clustering data into regions

--- DistanceMatrix.py
+++ DistanceMatrix.py
@@ -8,69 +8,36 @@ import time
 import csv
 import re
 
-def FormatParameters(parameters):
-    formatted_params = ""
-    for param in parameters:
-        split_string = param.split(',')
-        split_string.reverse()
-        param = ','.join(split_string)
-        formatted_params += param + "|"
-    return formatted_params[:-1]
+def ClusterData(building_data, minlat, maxlat, minlon, maxlon):
+    num_clusters = 22
+    lat_range = np.linspace(minlat, maxlat, num_clusters + 1)
+    lon_range = np.linspace(minlon, maxlon, num_clusters + 1)
+    aggregate_building_data = [{'id': 0, 'lat': 42.056732, 'lon': -87.687349, 'height': 0}];
+    node_id = 1
 
-    # pipe = "|"
-    # return pipe.join(parameters);
+    for ii in range(0,num_clusters):
+        for jj in range(0,num_clusters):
+            clusterdata = [x for x in building_data if lat_range[ii] <= x['lat'] <= lat_range[ii+1] \
+            and lon_range[jj] <= x['lon'] <= lon_range[jj+1]]
 
-def BuildRequest(origins, destinations, key):
-    origins = "origins=" + FormatParameters(origins) + "&"
-    destinations = "destinations=" + FormatParameters(destinations) + "&"
-    APIkey = "key=" + key
-    return "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&" + origins + destinations + APIkey
+            if clusterdata:
+                avg_lat = np.mean(map(lambda x: x['lat'], clusterdata))
+                avg_lon = np.mean(map(lambda x: x['lon'], clusterdata))
+                total_height = sum(map(lambda x: x['stories'], clusterdata))
 
-def ParseResponse(response):
-    response = json.loads(response.text)['rows']
-    numOrigins = len(response)
-    numDestinations = len(response[0]['elements'])
+                aggregate_building_data.append({'id': node_id, 'lat': avg_lat, 'lon': avg_lon, 'height': total_height})
+                node_id += 1
 
-    #columns = destinations = elements
-    #rows = origins = rows
-    distanceMatrix = np.zeros((numOrigins, numDestinations), dtype='f')
-    durationMatrix = np.zeros((numOrigins, numDestinations), dtype="int")
-
-    for o in range(0, numOrigins):
-        for d in range(0, numDestinations):
-
-            #duration in seconds
-            duration = response[o]['elements'][d]['duration']['value']
-            #distance in meters
-            distance = response[o]['elements'][d]['distance']['value']
-
-            durationMatrix[o, d] = duration
-            distanceMatrix[o, d] = distance
-    print "duration matrix: "
-    print durationMatrix
+                
+    print aggregate_building_data
+    print len(aggregate_building_data)
     print
-    print "distance matrix: "
-    print distanceMatrix
-
-def GetMatrix(origins, destinations, key):
-    max_request_params = 10;
-    num_requests_sqrt = int(math.ceil(len(origins)/float(max_request_params)))
-    for d in range(0, num_requests_sqrt):
-        current_destination = max_request_params * d
-        destinations_subset = destinations[current_destination:(current_destination + max_request_params)]
-        for o in range(0, num_requests_sqrt):
-            current_origin = max_request_params * o
-            origins_subset = origins[current_origin:(current_origin + max_request_params)]
-
-            HTTPRequest = BuildRequest(origins_subset, destinations_subset, key)
-            response = requests.get(HTTPRequest)
-            time.sleep(1)
-            print ParseResponse(response)
-            print
-    return ParseResponse(response)
-    # Concatenate number matrices
 
 def GetData():
+    minlat = 42.048164
+    maxlat = 42.062057
+    minlon = -87.710688
+    maxlon = -87.678947
     building_data = [];
     with open('Buildings.csv', 'rb') as f:
         reader = csv.reader(f)
@@ -94,17 +61,17 @@ def GetData():
                 lat = float(row[2].split(',')[1])
 
                 #check if lat and lon are in evanston range
-                if lat < 42.062057 and lat > 42.048164 and lon < -87.678947 and lon > -87.710688:
+                if lat < maxlat and lat > minlat and lon < maxlon and lon > minlon:
                     building_data.append({'lat': lat, 'lon': lon, 'stories': num_stories})
-    print building_data
-    print
+
+    ClusterData(building_data, minlat, maxlat, minlon, maxlon)
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         # origins = ["Vancouver, BC, Canada", "Seattle, WA, USA", "Victoria, BC, Canada"]
         # destinations = ["San Francisco, CA, USA", "Victoria, BC, Canada"]
 
-        GetData();
+        GetData()
 
     #else:
     #    origins = sys.argv[0]
