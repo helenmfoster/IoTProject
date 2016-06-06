ac4f9d169df38b12c988a4c0c055d69f3dd2b58c
= <helenfoster2016@u.northwestern.edu>
2016-06-05 21:22:32 -0500

    number of trucks showing up in side panel

--- app.py
+++ app.py
@@ -9,8 +9,14 @@ def main():
 
 @app.route('/truck_routes')
 def truck_routes():
-    routes = TruckRouter.get_routes()
-    return render_template('truck_routes.html')
+    ## routes = TruckRouter.get_routes()
+    ## Code Goes Here for turning routes into an array of arrays of tuples:
+    ##  a series of [lat, long]
+    #trucks = [[(42.0006, -87.6850), (42.0500, -87.6900)], [(82.0445, -87.6870), (42.0006, -87.6850), (42.0500, -87.6900)]]
+
+    trucks = [1, 2, 3, 4, 5]
+
+    return render_template('truck_routes.html', trucks = trucks)
 
 
 if __name__ == '__main__':
